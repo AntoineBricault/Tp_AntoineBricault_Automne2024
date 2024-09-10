59a6b7bd0a244d50315feee75c88c7a17ca98cb4
Antoine Bricault <2164070@cegepmontpetit.ca>
2024-09-10 16:00:19 -0400

    FCT : ajout d'une liste
    Ajout d'une liste de string temporaire

--- tp_automne_2024/lib/main.dart
+++ tp_automne_2024/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Accueil'),
     );
   }
 }
@@ -30,14 +30,20 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  List<String> ListeTache = [];
+  @override
+  void initState() {
+    for(var i = 0; i < 100; i++)
+    {
+      ListeTache.add(i.toString());
+    }
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -46,25 +52,15 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
+
+
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: ListView.builder(
+          itemCount: ListeTache.length,
+          itemBuilder: (context, index) {
+            return ListTile(title: Text('item #${ListeTache[index]}'));
+          },
+        )),// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
