9b029592bf40b18d6ed18521c3fbf0e681b42326
Antoine Bricault <2164070@cegepmontpetit.ca>
2024-09-19 13:45:24 -0400

    FCT: cookie
    Ajout de la gestion des cookies

--- tp_automne_2024/lib/lib_http.dart
+++ tp_automne_2024/lib/lib_http.dart
@@ -1,9 +1,24 @@
 import 'package:dio/dio.dart';
+import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:tp_automne_2024/transfert.dart';
+import 'package:cookie_jar/cookie_jar.dart';
+
+
+class SingletonDio {
+  static var cookiemanager = CookieManager(CookieJar());
+
+  static Dio getDio() {
+    Dio dio = Dio();
+    dio.interceptors.add(cookiemanager);
+    return dio;
+  }
+}
+
+
 
 Future<SignupResponse> signup(SignupRequest req) async {
   try {
-    var response = await Dio()
+    var response = await SingletonDio.getDio()
         .post('http://10.0.2.2:8080/api/id/signup', data: req.toJson());
     print(response);
     return SignupResponse.fromJson(response.data);
